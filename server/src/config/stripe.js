c6992579150d04b7db77db29b9562598c9dc7574
Caldeiraa <mirilo.bxd@gmail.com>
2026-09-09 20:15:49 -0300

    Limpa branch main

--- server/src/config/stripe.js
+++
@@ -1,7 +0,0 @@
-const Stripe = require('stripe');
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder', {
-  apiVersion: '2024-12-18.acacia',
-});
-
-module.exports = { stripe };
--- server/src/modules/payments/payments.schema.js
+++
@@ -1,8 +0,0 @@
-const { z } = require('zod');
-
-const createPaymentIntentSchema = z.object({
-  orderId: z.string().min(1, 'ID do pedido inválido'),
-  paymentMethod: z.enum(['CREDIT_CARD', 'DEBIT_CARD', 'PIX', 'BOLETO']).optional().default('CREDIT_CARD'),
-});
-
-module.exports = { createPaymentIntentSchema };
